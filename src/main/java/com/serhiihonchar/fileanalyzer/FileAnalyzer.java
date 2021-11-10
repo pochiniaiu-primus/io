f0d8abe0be402966fe91d6594d4ac2c01d28da72
Serhii Honchar <serhii.honchar.java@gmail.com>
2021-11-10 17:01:47 +0200

    refactoring FileAnalyzer

--- src/main/java/com/serhiihonchar/fileanalyzer/FileAnalyzer.java
+++ src/main/java/com/serhiihonchar/fileanalyzer/FileAnalyzer.java
@@ -1,26 +1,26 @@
-package com.serhiihonchar;
+package com.serhiihonchar.fileanalyzer;
 
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
 public class FileAnalyzer {
+    static File file;
+    static String word;
+
+    public static void main(String[] args) {
+        file = new File(args[0]);
+        word = args[1];
+        readingFromFile();
+    }
 
     public static void readingFromFile() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the path to the file where the word must be found: ");
-        String filename = scanner.nextLine();
-        File file = new File(filename);
+
         Charset charset = StandardCharsets.UTF_8;
         String content;
         int count = 0;
 
         try (InputStream inputStream = new FileInputStream(file)) {
-
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the word to be found: ");
-            String word = sc.nextLine();
 
             byte[] bytes = new byte[(int) file.length()];
 
@@ -43,13 +43,8 @@ public class FileAnalyzer {
             } else {
                 System.out.println("Word: " + word + " was found! Count : " + count);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        readingFromFile();
     }
 }
--- src/test/java/com/serhiihonchar/FileAnalyzerTest.java
+++ src/test/java/com/serhiihonchar/FileAnalyzerTest.java
@@ -1,11 +1,18 @@
 package com.serhiihonchar;
 
+import com.serhiihonchar.fileanalyzer.FileAnalyzer;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileAnalyzerTest {
+    FileAnalyzer analyzer = new FileAnalyzer();
 
+    public static void main(String[] args) {
+       FileAnalyzer.main(new String[]{"story.txt", "duck"});
+    }
 }
